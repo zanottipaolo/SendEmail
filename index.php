ba753e0985a30ca2089a12bdabbee5a06ab0cc18
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2020-09-28 18:49:33 +0200

    Add files

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Send Email</title>
+    <link rel="icon" href="img/email.ico">
+    <meta charset="UTF-8">
+    <meta name="description" content="Send your email message to everyone, just for fun.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+    integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src='https://www.google.com/recaptcha/api.js' async defer></script>
+  </head>
+  <body class="p-3 mb-2 bg-dark text-white">
+    <h1 class="text-center font-weight-bold pb-4">SEND EMAIL TO ANYONE</h1>
+
+    <!--Form principale in cui inserire i dati, verranno poi elaborati dallo script send.php-->
+    <form action="send.php" method="post" class="col-lg-4 pt-3 pb-3" style="margin: 0 auto; border-radius: 10px; border: 3px solid #dc3545;">
+      <div class="form-group">
+        <label for="sendto">To:</label>
+        <input type="email" class="form-control" name= "sendto" id="sendto" placeholder="Enter email">
+      </div>
+      <div class="form-group">
+        <label for="youremail">From:</label>
+        <input type="email" class="form-control" name= "youremail" id="youremail" placeholder="Enter your email">
+      </div>
+      <div class="form-group">
+        <label for="object">Object:</label>
+        <input type="text" class="form-control" name= "object" id="object" placeholder="Enter the object">
+      </div>
+      <div class="form-group">
+        <label for="msg">Message:</label>
+        <textarea class="form-control" name= "msg" id="msg" placeholder="Enter the message"></textarea>
+      </div>
+
+      <!--Google reCAPTCHA checkbox-->
+      <div class="text-center">
+        <div class="g-recaptcha" data-sitekey="_YOUR_SITE_KEY_" style="display: inline-block;">
+        </div>
+      </div>
+      <br>
+      <div class="text-center">
+        <button type="reset" class="btn btn-danger font-weight-bold"><i class="fa fa-remove" style="color: white;"></i></button>
+        <button type="submit" name="submit" class="btn btn-success font-weight-bold"><i class="fas fa-check" style="color: white;"></i></button>
+      </div>
+    </form>
+
+    <br />
+
+    <footer class="page-footer">
+      <div class="footer-copyright text-center py-3 font-weight-bold">
+        &copy; <?php echo date("Y");?> IntZeta Production | <a href="mailto:intzeta@gmail.com">Contact me</a>
+      </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ send.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Send Email</title>
+    <link rel="icon" href="img/email.ico">
+    <meta charset="UTF-8">
+    <meta name="description" content="Send your email message to everyone, just for fun.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+    integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body class="p-3 mb-2 bg-dark text-white">
+    <?php
+
+      // controllo della risposta dell'utente (chiamata POST, file JSON decodificato)
+      if(isset($_POST['g-recaptcha-response'])){
+        $captcha=$_POST['g-recaptcha-response'];
+        $secretKey = "_YOUR_SECRET_KEY_";
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
+        $response = file_get_contents($url);
+        $responseKeys = json_decode($response,true);
+
+        //Se il Google reCAPTCHA è stato risolto allora si può inviare l'email
+        if($responseKeys["success"]) {
+          if(isset($_POST['submit'])){
+            $to      = $_POST['sendto'];
+            $subject = $_POST['object'];
+            $message = $_POST['msg'];
+            $headers = 'From: ' . $_POST['youremail'] . "\n" . 'Reply-To: ' . $_POST['youremail'] . "\n" . 'X-Mailer: PHP/' . phpversion();
+
+            $sendmail = mail($to, $subject, $message, $headers);
+
+            //Controllo se l'email è stata inviata o meno
+            if($sendmail)
+              echo '<h3 class="text-center">Email sent</h3>';
+            else
+              echo '<h3 class="text-center">Email not sent, try again</h3>';
+          }
+        }
+
+        //se ci sono stati degli errori
+        else
+          echo '<h3 class="text-center">Sorry, try again :/</h3>';
+      }
+
+      //se non è stato completato il Google reCAPTCHA
+      if(!$captcha)
+        echo '<h3 class="text-center">Please check the captcha</h3>';
+    ?>
+
+    <br />
+
+    <p class="text-center"><a href="index.php">Back to home page</a></p>
+
+    <br />
+
+    <footer class="page-footer" style="position:fixed; bottom:0; left:0; right:0">
+      <div class="footer-copyright text-center py-3 font-weight-bold">
+        &copy; <?php echo date("Y");?> IntZeta Production | <a href="mailto:intzeta@gmail.com">Contact me</a>
+      </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
